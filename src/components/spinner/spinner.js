8725098e6e5db8562c7c4260f74e79cd9987514c
Maksim <makstrofi@yandex.ru>
2020-03-28 18:46:46 +0300

    added error indicator

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -5,8 +5,8 @@ import './spinner.css';
 const Spinner = () => {
 
 	return (
-		<div class="loadingio-spinner-double-ring-4ttoa11063g">
-			<div class="ldio-oa9eu91wtdm">
+		<div className="loadingio-spinner-double-ring-4ttoa11063g">
+			<div className="ldio-oa9eu91wtdm">
 				<div></div>
 				<div></div>
 				<div>
