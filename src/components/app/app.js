60658ed21cd30410d38a81decd56aa64d86cb983
Maksim <makstrofi@yandex.ru>
2020-03-30 00:51:49 +0300

    learning to use componenWillUnmount & componentDidCatch

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,40 +2,56 @@ import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
-
+import ErrorIndicator from '../error-indicator';
+import PeoplePage from '../people-page';
 
 import './app.css';
 
 export default class App extends Component {
 
   state = {
-    selectedPerson: null
+    showRandomPlanet: true,
+    hasError: false
   };
 
-  onPersonSelected = (id) => {
-    this.setState( {
-      selectedPerson: id
+  toggleRandomPlanet = () => {
+    this.setState((state) => {
+      return {
+        showRandomPlanet: !state.showRandomPlanet
+      }
     });
   };
 
-  render() {
-  return (
-    <div>
-      <Header />
-      <RandomPlanet />
-      
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList onItemSelected={ this.onPersonSelected }/>
-        </div>
-        <div className="col-md-6">
-          <PersonDetails personId={ this.state.selectedPerson }/>
-        </div>
-      </div>
-    </div>
-  );
-};
+  componentDidCatch() {
+    this.setState({ hasError: true });
+  }
 
-};
+  render() {
+
+    if (this.state.hasError) {
+      return <ErrorIndicator />
+    }
+
+    const planet = this.state.showRandomPlanet ?
+      <RandomPlanet/> :
+      null;
+
+    return (
+      <div className="stardb-app">
+        <Header />
+        { planet }
+
+        <div className="row mb2 button-row">
+          <button
+            className="toggle-planet btn btn-warning btn-lg"
+            onClick={this.toggleRandomPlanet}>
+            Toggle Random Planet
+          </button>
+        </div>
+
+        <PeoplePage />
+
+      </div>
+    );
+  }
+}
