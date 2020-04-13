7e12e90b6fe5802ff45b92c251fb3d6b683cb6bb
Maksim <makstrofi@yandex.ru>
2020-04-13 21:21:22 +0300

    learning to use propTypes

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -8,6 +8,22 @@ import './random-planet.css';
 
 export default class RandomPlanet extends Component {
 
+  static defaultProps = {
+    updateInterval: 10000
+  };
+
+  static propTypes = {
+    updateInterval: (props, propName, componentName) => {
+      const value = props[propName];
+
+      if (typeof value === 'number' && !isNaN(value)) {
+        return null;
+      }
+
+      return new TypeError(`${componentName}: ${propName} must be number`);
+    }
+  };
+
   swapiService = new SwapiService();
 
   state = {
@@ -65,9 +81,6 @@ export default class RandomPlanet extends Component {
       </div>
     )
   };
-    static defaultProps = {
-      updateInterval: 10000
-    };
 }
 
 
